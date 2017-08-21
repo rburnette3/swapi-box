e87bc96cb2820a36a099f5f68e8d55e8ece20d01
Ralph Burnette <rgburnette3@gmail.com>
2017-08-21 17:44:28 -0600

    file structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from '../logo.svg';
 
 class App extends Component {
+  constructor(){
+    super()
+
+
+  }
+
   render() {
     return (
       <div className="App">
