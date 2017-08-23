c24937675809818412c2b303bbdd8d72d06e9bc7
Endless Hypnosis <endlesshypnosis@gmail.com>
2017-08-22 20:45:41 -0600

    people and homeworld together

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -6,11 +6,11 @@ export default class API {
   }
 
   actualFetch(pageNum) {
-    fetch(`https://swapi.co/api/${this.type}/?page=${pageNum}`)
+    return fetch(`https://swapi.co/api/${this.type}/?page=${pageNum}`)
       .then(result => result.json())
       .then(jsonResult => {
         if (jsonResult.next) {
-          console.log('FIRST WUT:', jsonResult);
+          // console.log('FIRST WUT:', jsonResult);
           this.bigArray = [...this.bigArray, ...jsonResult.results]
           return this.actualFetch(pageNum + 1)
         } else {
@@ -26,9 +26,35 @@ export default class API {
     let fallback = 0;
     let finalDataSet = [];
 
-    this.actualFetch(1)
-    console.log('WUT:', wut);
-    console.log('big array:', this.bigArray);
+    this.actualFetch(1).then(result => {
+
+      let arrayOfPromises;
+
+      if (this.type === 'people') {
+        arrayOfPromises = this.bigArray.map(item => {
+          return fetch(item.homeworld)
+            .then(result => result.json())
+        })
+
+        let finalArray = Promise.all(arrayOfPromises)
+          .then(result => {
+            console.log('what is promise result', result);
+            return result.map((homeworld, i) => {
+              return Object.assign({}, this.bigArray[i], homeworld)
+            })
+          })
+          .then(result => {
+            console.log('UHHUH:', result);
+          })
+
+          console.log('what is final array:', finalArray);
+
+      }
+
+      console.log('big array:', this.bigArray);
+    })
+
+    // console.log('WUT:', wut);
     //
     // while(keepGoing && fallback < 11) {
     //
